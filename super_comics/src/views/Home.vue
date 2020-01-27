1d4ec31d1f04a1279f16ffd198c5c7f63ca99db7
Jeremyrts <jeremyrts@Pumba.local>
2020-01-27 16:39:36 +0100

    Home view ready

--- super_comics/src/views/Home.vue
+++ super_comics/src/views/Home.vue
@@ -1,37 +1,54 @@
 
 <template>
   <div class="home">
-    <h1>Homepage </h1>
-    {{next_choice}}
+    <h1>Super Comics </h1>
+    <Button/>
+    <!-- {{next_choice}} -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import db from '../../base'
+import Button from '@/components/Button'
 
 export default {
   name: 'home',
   components: {
+    Button
   },
-  data() {
-    return {
-      next_choice: null,
-      currentActionId: 0
-    }
-  },
+  // data() {
+  //   return {
+  //     next_choice: null,
+  //     currentActionId: 0
+  //   }
+  // },
 
-  firebase: {
-    next_choice: db.ref('SuperComics/next_choice')
-  },
+  // firebase: {
+  //   next_choice: db.ref('SuperComics/next_choice')
+  // },
 
-  watch: {
-    next_choice: {
-      handler() {
+  // watch: {
+  //   next_choice: {
+  //     handler() {
 
-        console.log(this.next_choice.timeout)
-      }
-    }
-  }
+  //       console.log(this.next_choice.timeout)
+  //     }
+  //   }
+  // }
 }
 </script>
+
+<style scoped>
+  h1 {
+    font-family: 'Bangers', cursive;
+    font-size: 5em;
+    color: #F9F9F9;
+    text-shadow: 2px 4px #FFC700;
+    text-align: center
+  }
+  .home {
+    display: flex;
+    flex-direction: column;
+    height: 100%
+  }
+</style>
