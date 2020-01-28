045c0ae0894b841069c735480f87b116f79fa2b8
Axel Donadio <axel@agence-revolutions.com>
2020-01-28 11:36:57 +0100

    queques trucs de pwa

--- super_comics/src/router/index.js
+++ super_comics/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home'
 import ChoiceSelector from '../views/ChoiceSelector'
+import CinemaMode from '../views/CinemaMode'
+import TransitionChoice from '../views/TransitionChoice'
 
 Vue.use(Router)
 const router = new Router({
@@ -21,15 +23,12 @@ const router = new Router({
     {
       path: '/cinemaMode',
       name: 'cinemaMode',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/CinemaMode')
+      component: CinemaMode
     },
     {
       path: '/transitionChoice',
       name: 'transitionChoice',
-      component: () => import('../views/TransitionChoice'),
+      component: TransitionChoice,
       props: true
     }
   ]
--- super_comics/src/views/Home.vue
+++ super_comics/src/views/Home.vue
@@ -35,8 +35,6 @@ export default {
   watch: {
     next_choice: {
       handler() {
-
-        console.log(this.next_choice.timeout)
       }
     }
   },
